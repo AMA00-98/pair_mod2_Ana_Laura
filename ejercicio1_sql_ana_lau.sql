a13ea4264b65f4fd4c173ffc1610f379221663e7
Ana Marín Aguirre <ama@MacBook-Air-de-Ana.local>
2026-03-02 12:03:34 +0100

    ejercicio 1

--- ejercicio1_sql_ana_lau.sql
+++ ejercicio1_sql_ana_lau.sql
@@ -3,12 +3,12 @@ USE northwind;
 -- Productos más baratos y caros de nuestra la bases de datos
 
 SELECT MAX(UnitPrice) AS highestPrice, MIN(UnitPrice) AS lowestPrice
-FROM products;                                    -- 263.5000	2.5000
+FROM products; -- Resultado: 263.5000 | 2.5000
 
 -- Conociendo el numero de productos y su precio medio
 
 SELECT COUNT(ProductName), AVG (UnitPrice)
-FROM products;                             -- resultado 77	28.86636364
+FROM products; -- Resultado: 77 | 28.86636364
 
 
 -- Sacad la máxima y mínima carga de los pedidos de UK
@@ -19,15 +19,33 @@ WHERE ShipCountry;
 
 SELECT MAX(Freight) , MIN(Freight) 
 FROM orders
-WHERE shipcountry= "uk";                       -- resultado 288.4300	0.9000
+WHERE shipcountry= "uk"; -- Resultado: 288.4300	| 0.9000
 
--- Qué productos se venden por encima del precio medio
+-- Qué productos se venden por encima del precio medio (A esto se le llama 'Subconsultas')
 
 SELECT  ProductName, UnitPrice
 FROM products
 WHERE UnitPrice > 28.86636364
+ORDER BY UnitPrice DESC; -- Resultado: - (Datos masivos)
+
+-- Qué productos se venden por encima del precio medio -> Manera correcta según el profesor ahora mismo
+
+SELECT AVG(UnitPrice)
+FROM products;
+
+SELECT *
+FROM products
+WHERE UnitPrice > 28.86636364
 ORDER BY UnitPrice DESC;
+
+-- Qué productos se han descontinuado
 
 SELECT COUNT(*)
 FROM products
-WHERE DISCONTINUED = 1;                      -- RESULTADO 8
+WHERE DISCONTINUED = 1; -- Resultado: 8
+
+-- Qué productos se han descontinuado -> Manera correcta según el profesor
+
+SELECT COUNT(UnitPrice)
+FROM products
+WHERE DISCONTINUED = 1; 
