898b44c84c0d88e7779a7139b5e6cc6ed3698b82
Ana Marín Aguirre <ama@MacBook-Air-de-Ana.local>
2026-03-03 19:31:54 +0100

    ejercicio 2 modificado

--- ejercicio2_sql_join_ana_lau.sql
+++ ejercicio2_sql_join_ana_lau.sql
@@ -2,28 +2,25 @@ USE Northwind;
 
 -- Pedidos por empresa en UK:
 
-SELECT * -- Consulto primero los clientes de UK
+SELECT * -- Consulto primero los clientes de UK 📌
     FROM Customers
     WHERE Country = 'UK';
 
--- Nos piden el ID del cliente y el nombre de la empresa y el número de pedidos
+-- Nos piden el ID del cliente y el nombre de la empresa de los clientes (TABLA 1)
 
-SELECT CustomerID, CompanyName  -- Veo la tabla con el id y nombre de la empresa 
-    FROM Customers
+SELECT CustomerID, CompanyName  -- Veo la tabla con el id y nombre de la empresa 📌
+    FROM Customers;
 
--- Tengo que unir el numero de pedidos
+-- Tengo que unir el numero de pedidos (TABLA 2)
 
-SELECT ProductID
-    FROM products 
+SELECT OrderID -- Veo el id de los pedidos 📌
+    FROM Orders; 
 
 -- Resultado uniendo todo lo de arriba con JOIN
 
-SELECT                                               -- no me sale
-    c.CustomerID,
-    c.CompanyName,
-    o.OrderID
-    FROM Customers AS c
-        INNER JOIN Orders AS o
-            ON c.CustomerID = o.CustomerID;
-    SELECT c.CustomerID,c.CompanyName,COUNT(o.OrderID) AS NumberOfOrders
-    FROM Customers AS c
+SELECT c.CustomerID AS Identificador, c.CompanyName AS NombreEmpresa, o.OrderID AS NumeroPedidos -- Seleccionamos las 2 columnas (Tab.1) y la columna (Tab.2) + Alias
+    FROM Customers AS c -- De la Tabla 1
+        INNER JOIN Orders AS o -- Y lo unimos con la Tabla 2
+            ON c.CustomerID = o.CustomerID -- Donde 'CustomerID' = FK de ambas tablas, coinciden
+	WHERE c.Country = "UK"; -- En donde los clientes pertenecen al país UK
+   
